9a2951b10c7c1dfb0e6fbd4ff166b93ac5d4ef70
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-18 11:07:36 -0300

    refactor: update import statements in Cliente class for improved clarity and functionality

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.net.*;
-import java.util.logging.Logger;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Cliente {
     private static final String HOST = "127.0.0.1";
